f1c437b486251797838c1be5c6bb42a14f541d44
Lancy Norbert Fernandes <lancy98@gmail.com>
2022-11-03 11:44:04 +1300

    CI integration
    CI integration

---
+++ brain-marks/Secrets-CI.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets-CI.swift
+//  brain-marks
+//
+//  Created by Lancy on 11/03/22.
+//
+
+import Foundation
+
+// swiftlint:disable line_length
+
+enum Secrets {
+    static let bearerToken = "paste-your-twitter-api-key-here-and-replace-this-string--example: aaaaaaapapppppiiiiiiiiiKeeeeeeeeyyyyyyyyy"
+}
